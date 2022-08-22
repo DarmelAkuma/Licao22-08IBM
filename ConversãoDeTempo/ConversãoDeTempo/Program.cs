4167123fae40ef80dab16f7dcb33e66f34a80aa5
Mateus Melo Oliveira <Mateus99Melo@gmail.com>
2022-08-22 10:53:50 -0300

    Inicio do projeto

---
+++ ConversãoDeTempo/ConversãoDeTempo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConversãoDeTempo {
+    class Program {
+        static void Main(string[] args) {
+
+            for (int i = 0; i < 3; i++) {
+
+                Console.WriteLine("Digite quantos segundos deseja converter: ");
+
+                int N, segundos, minutos, horas, sobra;
+
+                N = int.Parse(Console.ReadLine());
+
+                horas = N / 3600;
+                sobra = N % 3600;
+                minutos = sobra / 60;
+                segundos = sobra % 60;
+
+                Console.WriteLine(horas + ":" + minutos + ":" + segundos);
+            }
+        }
+    }
+}
